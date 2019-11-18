90041cb88b20e635c0ddbd2fd9dba89a510d5b72
Denis Sokolov <denis@sokolov.cc>
2019-11-18 12:00:00 +0200

    Refactor

--- src/open/index.js
+++ src/open/index.js
@@ -1,18 +1,31 @@
-(function() {
-  function encode(str) {
-    return str
+(async function() {
+  const api = (function() {
+    const promisify = f => (...args) =>
+      new Promise(resolve => f(...args, resolve));
+    const flatten = bookmarks =>
+      [bookmarks]
+        .concat(bookmarks.map(b => (b.children ? flatten(b.children) : [])))
+        .flat();
+    const getSubTree = promisify(chrome.bookmarks.getSubTree);
+    const getTree = promisify(chrome.bookmarks.getTree);
+    return {
+      allDescendantsOfId: async folderId => flatten(await getSubTree(folderId)),
+      getFullTree: async function() {
+        const bookmarks = await getTree();
+        // Chrome returns an empty top-level entry
+        if (bookmarks.length === 1 && !bookmarks[0].title)
+          return bookmarks[0].children;
+        return bookmarks;
+      }
+    };
+  })();
+
+  const encode = str =>
+    str
       .replace(/&/g, "&amp;")
       .replace(/</g, "&lt;")
       .replace(/>/g, "&gt;")
       .replace(/"/g, "&quot;");
-  }
-
-  function flatten(bookmarks) {
-    return [bookmarks]
-      .concat(bookmarks.map(b => (b.children ? flatten(b.children) : [])))
-      .flat();
-  }
-
   const random = list => list[Math.floor(Math.random() * list.length)];
 
   function treeHtml(bookmarks) {
@@ -43,26 +56,19 @@
   const params = new URLSearchParams(window.location.search);
   const folderId = params.get("folderId");
   if (folderId) {
-    chrome.bookmarks.getSubTree(folderId, function(bookmarks) {
-      const bookmark = random(
-        flatten(bookmarks)
-          .filter(b => b.url)
-          .filter(
-            b => !b.url.includes(chrome.runtime.getURL("src/open/index.html"))
-          )
-      );
-      if (!bookmark) throw new Error("No bookmarks in this folder");
-      window.location.href = bookmark.url;
-    });
+    const bookmarks = await api.allDescendantsOfId(folderId);
+    const bookmark = random(
+      bookmarks
+        .filter(b => b.url)
+        .filter(
+          b => !b.url.includes(chrome.runtime.getURL("src/open/index.html"))
+        )
+    );
+    if (!bookmark) throw new Error("No bookmarks in this folder");
+    window.location.href = bookmark.url;
   } else {
-    chrome.bookmarks.getTree(function(bookmarks) {
-      // Chrome returns an empty top-level entry
-      if (bookmarks.length === 1 && !bookmarks[0].title)
-        bookmarks = bookmarks[0].children;
-
-      const div = document.createElement("div");
-      div.innerHTML = treeHtml(bookmarks);
-      document.body.appendChild(div);
-    });
+    const div = document.createElement("div");
+    div.innerHTML = treeHtml(await api.getFullTree());
+    document.body.appendChild(div);
   }
 })();
